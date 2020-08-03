17930c1b295152f3f39bf028268749310ff5311b
Ann Hagan <ann.marie783@gmail.com>
2020-08-03 17:30:57 -0500

    Adding SurfaceVolume Property

--- Program.cs
+++ Program.cs
@@ -8,16 +8,18 @@ namespace boxProgram
         {
             //create an object of the class box.cs
 
-            box box = new box();
+            //box box = new box();
 
             //assign the values to the object
-            box.Height = -3;
+            //box.Height = -3;
             //box.length = 3;
-            box.SetLength(4);
-            box.Width = 14;
+            ///box.SetLength(4);
+            //box.Width = 14;
+            box box = new box(3, 4, 5);
             Console.WriteLine("Box length is " + box.GetLength() + " and the volume is " + box.GetVolume());
             Console.WriteLine("Box width is " + box.Width);
             box.DisplayInfo();
+            Console.WriteLine("Front surface is {0}", box.FrontSurface);
         }
     }
 }
--- box.cs
+++ box.cs
@@ -4,6 +4,8 @@ namespace boxProgram
 {
     public class box
     {
+        //Created member varialbes 
+        //made them private which requires new properties to access such as set and get.
         private int length = 3;
         public int height;
         public int width;
@@ -33,7 +35,27 @@ namespace boxProgram
             }
         }
 
-        //creating a method to set a variable
+//user properties must have capital Letter "Volume"  use either get or set
+        public int Volume 
+        {
+            get{
+                return volume;
+            }
+            set
+            {
+                this.volume = value;
+            }
+        }
+
+        // create a constuctor
+        public box(int lenth, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            this.width = width;
+        }
+
+        //creating a property to set a variable
         //changing the public to private will not allow outside programs to set the length
         public void SetLength(int length) 
         {
@@ -48,6 +70,12 @@ namespace boxProgram
         public int GetVolume ()
         {
             return this.length * this.height * this.width;
+        }
+
+        //new property to caluclate a surface
+        public int FrontSurface 
+        {
+            get { return height * length;}
         }
         public void DisplayInfo() {
             Console.WriteLine("Length is {0}, heigth is {1} width is {2}.  Volume is {3}.", length, height, width, volume = height * width * length);
